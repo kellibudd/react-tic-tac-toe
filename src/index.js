2e3774ebc0da9c8374056857dd90eea7d7404ff0
Kelli Budd <kelli.budd@floqast.com>
2020-09-25 09:52:57 -0700

    Lift state to Game component to store historical moves

--- src/index.js
+++ src/index.js
@@ -2,30 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import './index.css'
 
-// class Square extends React.Component {
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             value: null
-//         };
-//     }
-
-//     render() {
-//         // TODO: use onClick={this.props.onClick}
-//         // TODO: replace this.state.value with this.props.value
-//         return (
-//             <button 
-//             className="square" 
-//             onClick={() => this.props.onClick()}
-//             /* Square calls this.handleClick(i) when clicked - the onClick event handler on the button
-//             calls on the onClick prop passed down by the Board (parent) component
-//             which ultimately calls the handleClick method */
-//             >
-//                 {this.props.value}
-//             </button>
-//         );
-//     }
-// }
 function Square(props) {
     return (
         <button 
@@ -41,52 +17,86 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            xIsNext: true,
-            squares: Array(9).fill(null)
-            /* Lifting state -- now state is stored in the Board (parent) component
-            instead of the individual Square components. The Square components receive values from the Board component
-            and inform the Board component when they’re clicked - they are now considered controlled components */
-        };
-    }
-
-    handleClick(i) {
-        const updatedSquaresArray = this.state.squares.slice()
-        /* slice() method returns a copy of the selected elements in an array, as a new array object.
-        we can modify a copy of the squares array instead of modifying the existing array - so we aren't directly modifying state*/
-        if (calculateWinner(updatedSquaresArray) || checkIfGameOver(updatedSquaresArray) || updatedSquaresArray[i]) {
-            return;
-            /* ignores a click if a square is already filled or if someone has won the game */
-        }
-        
-        updatedSquaresArray[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            xIsNext: !this.state.xIsNext,
-            squares: updatedSquaresArray})
-        /* Data Change without Mutation - changing the value of the clicked element in the array copy from null to 'X',
-        then replacing the data attached to state with a new copy which has the desired changes.
-        Avoiding direct data mutation lets us keep previous versions of the game’s history intact, and reuse them later.
-        Immutable data can easily determine if changes have been made, which helps to determine when a component
-        requires re-rendering. */
-    }
     
-    renderSquareFunction(i) {
-        
+    renderSquare(i) {
+        /* this is a custom method that is rendering the square component - could be named anything */
         return (
             <Square 
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
 
     render() {
-        const winner = calculateWinner(this.state.squares);
-        const tie = checkIfGameOver(this.state.squares);
-        let status;
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+    );
+  }
+}
 
+class Game extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true
+        };
+    }
+
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1]
+        const updatedSquares = current.squares.slice()
+        /* slice() method returns a copy of the selected elements in an array, as a new array object.
+        we can modify a copy of the squares array instead of modifying the existing array - so we aren't directly modifying state*/
+        if (calculateWinner(updatedSquares) || checkIfGameOver(updatedSquares) || updatedSquares[i]) {
+            return;
+            /* ignores a click if a square is already filled or if someone has won the game */
+        }
+        
+        updatedSquares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            xIsNext: !this.state.xIsNext,
+            history: history.concat([{
+                /* concatenate new history entries onto history - the concat method doesn’t mutate the original array */
+                squares: updatedSquares
+            }])
+        });
+        /* 
+        Data Change without Mutation - changing the value of the clicked element in the array copy from null to 'X',
+        then replacing the data attached to state with a new copy which has the desired changes.
+        Avoiding direct data mutation lets us keep previous versions of the game’s history intact, and reuse them later.
+        Immutable data can easily determine if changes have been made, which helps to determine when a component
+        requires re-rendering. */
+    }
+
+    render() {
+        const history = this.state.history;
+        const current = history[history.length - 1]
+        /* with current - using the most recent history entry to determine and display the game’s status */
+        const winner = calculateWinner(current.squares);
+        const tie = checkIfGameOver(current.squares);
+
+        let status;
         if (winner) {
             status = 'Winner: ' + winner;
         } else if (tie) {
@@ -96,37 +106,15 @@ class Board extends React.Component {
         }
 
         return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquareFunction(0)}
-                    {this.renderSquareFunction(1)}
-                    {this.renderSquareFunction(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquareFunction(3)}
-                    {this.renderSquareFunction(4)}
-                    {this.renderSquareFunction(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquareFunction(6)}
-                    {this.renderSquareFunction(7)}
-                    {this.renderSquareFunction(8)}
-                </div>
-            </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-    render() {
-        return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board 
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                <div>{/* status */}</div>
+                <div>{status}</div>
                 <ol>{/* TODO */}</ol>
                 </div>
             </div>
